5e92fa6e9abf25b2ec74efe36a2645ad844b3d94
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-08-05 00:23:55 +0200

    Convert idf files to map files - l3

--- convert_idf_to_map/making_map_from_idf.py
+++ convert_idf_to_map/making_map_from_idf.py
@@ -12,6 +12,7 @@ import raster_func
 # input directory containing idf file
 inp_folder = "/scratch-shared/edwinhs/tristan_data/Colombia/head/l1/"
 inp_folder = "/scratch-shared/edwinhs/tristan_data/Colombia/head/l2/"
+inp_folder = "/scratch-shared/edwinhs/tristan_data/Colombia/head/l3/"
 
 # list of idf files
 idf_files  = glob.glob(inp_folder + "/*")
@@ -19,6 +20,7 @@ idf_files  = glob.glob(inp_folder + "/*")
 # output directory
 out_folder = "/scratch-shared/edwinhs/colombia_model_results/head/l1_top/"
 out_folder = "/scratch-shared/edwinhs/colombia_model_results/head/l2_mid/"
+out_folder = "/scratch-shared/edwinhs/colombia_model_results/head/l3_bot/"
 
 # making output directory
 try:
