70d3dadc355677efe43b1372363413380595f33d
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-08-07 01:07:03 +0200

    Start evaluating.

--- comparing_and_evaluating_maps/compare_and_evaluate.py
+++ comparing_and_evaluating_maps/compare_and_evaluate.py
@@ -17,6 +17,9 @@ pcr.setclone(clone_map_filename)
 str_year = 2000
 end_year = 2012
 
+# output folder location
+output_folder = "/scratch-shared/edwinhs/tristan_output/evaluation/"
+
 # calculate the average value/map of the local model
 msg = "Calculate the average value/map of the local model!"
 print(msg)
@@ -26,41 +29,41 @@ i_month = 0
 cum_map = pcr.scalar(0.0)
 for year in range(str_year, end_year + 1, 1):
     for month in range(1, 12 + 1, 1):
-        i_month   = i_month + 1
+        # file name, e.g. "/scratch-shared/edwinhs/colombia_model_results/head/l1_top/head_20000201_l1.idf.map"
         file_name = "head_%04d%02d" %(year, month)
         file_name = local_model_folder + "/" + file_name + "01_l1.idf.map"   
         print(file_name)
+        # cummulative values
         cum_map   = cum_map + pcr.readmap(file_name)
+        i_month   = i_month + 1
+# calculating average and saving it to a pcraster map
 average_local = cum_map / i_month
 pcr.aguila(average_local)
+pcr.report(average_local, output_folder + "/average_local.map")
 
-#~ # calculate the average value/map of the global model
-#~ # - Using the upper layer of the global model
-#~ global_model_folder = "/scratch-shared/edwinhs/modflow_results_in_pcraster/upper_layer/regional/" 
-#~ i_month = 0
-#~ for year in (str_year, end_year + 1, 1):
-    #~ for month in (1, 12 + 1, 1):
-#~ average_global = 
-#~ 
-#~ # calculate the anomaly value of the local model
-#~ anomaly_local  = {}
-#~ 
-#~ 
-#~ # calculate the anomaly value of the global model
-#~ anomaly_global = {}
-#~ 
-#~ 
-#~ # calculate the climatology values of the local  model's anomaly value
-#~ climatology_anomaly_local = 
-#~ for month in (1, 12 + 1, 1):
-#~ 
-#~ 
-#~ # calculate the climatology values of the global model's anomaly value
-#~ climatology_anomaly_global = 
-#~ for month in (1, 12 + 1, 1):
-#~ 
-#~ 
-#~ # evaluating climatology maps (e.g. calculate the differences in climatology maps) 
-#~ difference = map_1 - map_2
-#~ for month in (1, 12 + 1, 1):
-#~ 
+# calculate the average value/map of the global model
+msg = "Calculate the average value/map of the global model!"
+print(msg)
+# - Using the top layer of the global model
+global_model_folder  = "/scratch-shared/edwinhs/modflow_results_in_pcraster/upper_layer/regional/" 
+i_month = 0
+cum_map = pcr.scalar(0.0)
+for year in range(str_year, end_year + 1, 1):
+    for month in range(1, 12 + 1, 1):
+        # file name, e.g. /scratch-shared/edwinhs/modflow_results_in_pcraster/upper_layer/regional/htop_2000_01.map
+        file_name = "htop_%04d_%02d" %(year, month)
+        file_name = global_model_folder + "/" + file_name + ".map"   
+        print(file_name)
+        # cummulative values
+        cum_map   = cum_map + pcr.readmap(file_name)
+        i_month   = i_month + 1
+# calculating average and saving it to a pcraster map
+average_global = cum_map / i_month
+pcr.aguila(average_global)
+pcr.report(average_global, output_folder + "/average_global.map")
+
+# evaluating/comparing two maps 
+difference = average_local - average_global
+pcr.aguila(difference)
+pcr.report(difference, output_folder + "/bias.map")
+
