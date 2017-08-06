4edf710761877f82a46df530f3f2d2922ef3f195
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-08-07 01:57:31 +0200

    Making scatterplots.

---
+++ make_scatterplots/make_scatterplots.R
@@ -0,0 +1,17 @@
+
+require(rgdal)
+
+# file names for input maps
+x_input_map_filename = "/scratch-shared/edwinhs/tristan_output/evaluation/average_global.map"
+y_input_map_filename = "/scratch-shared/edwinhs/tristan_output/evaluation/bias.map"
+
+# reading map values
+x_values = readGDAL(x_input_map_filename)
+y_values = readGDAL(y_input_map_filename)
+
+# plot values
+output_plot_filename = "/scratch-shared/edwinhs/tristan_output/plot_bias.pdf"
+pdf(output_plot_filename)
+par(bg = "white")
+plot(x_values$band1, y_values$band1)
+dev.off()
