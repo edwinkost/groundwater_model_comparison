362623776401c59b4a7314f38a159bfd7a607c5f
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-08-07 02:24:18 +0200

    finalizing

--- make_scatterplots/make_scatterplots.R
+++ make_scatterplots/make_scatterplots.R
@@ -10,7 +10,7 @@ x_values = readGDAL(x_input_map_filename)
 y_values = readGDAL(y_input_map_filename)
 
 # plot values
-output_plot_filename = "/scratch-shared/edwinhs/tristan_output/plot_bias.pdf"
+output_plot_filename = "/scratch-shared/edwinhs/tristan_output/evaluation/plot_bias.pdf"
 pdf(output_plot_filename)
 par(bg = "white")
 plot(x_values$band1, y_values$band1)
